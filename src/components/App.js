814bc28e491d490ed7235aa4d6cbf8bb5254ab87
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-01-22 22:41:56 +0800

    add new page AuthorCommentPage to App

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import ChannelMenu from './ChannelMenu';
 import { ChannelVideo } from './ChannelVideo';
 import VideoDetails from './videoDetail';
-
+import AuthorCommentPage from './AuthorCommentsPage';
 
 const App = () => {
   return (
@@ -13,6 +13,7 @@ const App = () => {
           <Route path="/eyescomment" exact component={ChannelMenu} />
           <Route path="/eyescomment/channel/:id" exact component={ChannelVideo} />
           <Route path="/eyescomment/video/:id" exact component={VideoDetails} />
+          <Route path="/eyescomment/author/:id" exact component={AuthorCommentPage} />
         </div>
       </BrowserRouter>
     </div>
